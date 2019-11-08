e8bb93802405b607fbd9925ec3e85470458e1bd4
yaoyu bao <yaobao5354@gmail.com>
2019-11-08 00:55:23 +0000

    add listing

--- lib/steam_deals/cli.rb
+++ lib/steam_deals/cli.rb
@@ -1,5 +1,20 @@
 class SteamDeals::CLI
   def call
-    puts "Welcome to Steam Deals!"
+    puts "\nWelcome to Steam Deals!"
+    get_current_categories
+    get_user_category
+    #get_games_for(category)
+    #list
   end
+
+  def get_current_categories
+    #to be scraped
+    @categories = ["New and Trending", "Top Selling", "Popular Upcoming", "Specials"]
+  end
+
+  def get_user_category
+    #list category
+    @categories.each.with_index(1){|category, index| puts "#{index}. #{category}"}
+  end
+
 end
