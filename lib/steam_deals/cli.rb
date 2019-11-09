e6ecc029637d9f76bc27e86ee309003c9cff3780
yaoyu bao <yaobao5354@gmail.com>
2019-11-09 18:50:35 +0000

    add category

--- lib/steam_deals/cli.rb
+++ lib/steam_deals/cli.rb
@@ -2,8 +2,10 @@ class SteamDeals::CLI
   def call
     puts "\nWelcome to Steam Deals!"
     get_current_categories
-    get_user_category
-    #get_games_for(category)
+    list_categories
+    get_user_input
+    create_deals_hash(@category)
+    display_games(@deals_hash)
     #list
   end
 
@@ -12,9 +14,22 @@ class SteamDeals::CLI
     @categories = ["New and Trending", "Top Selling", "Popular Upcoming", "Specials"]
   end
 
-  def get_user_category
+  def list_category
     #list category
     @categories.each.with_index(1){|category, index| puts "#{index}. #{category}"}
   end
 
+  def get_user_input
+    puts "\nplease enter a category number"
+    @category = get.strip().to_i
+  end
+
+  def create_deals_hash(category)
+    categories = ["tab_newreleases_content", "tab_topsellers_content", "tab_upcoming_content", "tab_specials_content"]
+    #to_scrap = categories[@category - 1]
+    #call scraper here with to scrap
+    @deals_hash = {:name => "game1", :genre => "genre1", :price =>"price1"}
+  def display_games(deals_hash)
+    
+  end
 end
